fd07cd375252703bf3b4816dffa8e7c67d0fb731
Mustafaa8a <mostpha.mo2006@gmail.com>
2025-12-03 23:48:06 +0200

    commit 2

--- FUZZ FrameWork Site/app.py
+++ FUZZ FrameWork Site/app.py
@@ -1,7 +1,16 @@
-from flask import Flask,render_template,render_template_string,send_from_directory,request
+from flask import Flask,render_template,render_template_string,send_from_directory,request,redirect,url_for
 from os import popen
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
 import urllib.parse
+
 app=Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'lol_i_am_the_admin'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+
+jwt=JWTManager(app)
 
 # Command injection filter 
 def filter(cmd):
@@ -20,6 +29,10 @@ def home():
 @app.route("/template")
 def template():
     name = request.args.get("name")
+    if name:
+        if '{' in name or '}' in name:
+            return render_template_string("Hacker",status_word="Hacker"), 403
+
     html=f"""
     <!DOCTYPE html>
     <html>
@@ -28,7 +41,7 @@ def template():
     </body>
     </html>
     """
-    return render_template_string(html,name=name)
+    return render_template_string(html)
 
 # Command injection challenge
 @app.route("/run")
@@ -41,10 +54,25 @@ def about():
     
     allowed = filter(cmd)
     if not allowed:
-        return render_template("run.html",output="Hacker")
+        return render_template("run.html",output="Hacker"), 403
     
     output=popen(f"ping -c 2 {cmd}").read()
     return render_template("run.html",output=f"{output}")
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    elif request.method=="POST":
+
+        if not username or not password:
+            return jsonify({"error": "Email and password required"}), 400
+
+        username = request.form["username"]
+        password = request.form["password"]
+        if username=="admin" and password=="admin":
+            return redirect(url_for("home"))
+
 
 if __name__=="__main__":
     app.run(debug=True,host='0.0.0.0', port=5000)
--- FUZZER/fuzz.py
+++ FUZZER/fuzz.py
@@ -1,12 +1,6 @@
-import argparse,requests,pyfiglet,colorama
+import argparse,requests,pyfiglet,colorama,re
 from pyfiglet import figlet_format
 from colorama import Fore, Style, init
-
-init()
-
-ascii_art = figlet_format('F U Z Z E R')
-print(Fore.GREEN + ascii_art + Style.RESET_ALL)
-print("-------------------------------------------------")
 
 
 # Create parser
@@ -14,21 +8,95 @@ parser = argparse.ArgumentParser()
 
 # Define arguments
 parser.add_argument("-u", "--url", help="Target URL", required=True)
-parser.add_argument("-w", "--wordlist", help="Path to wordlist", required=True)
-parser.add_argument("-b", "--cookie", help="Cookie header (optional)")
+parser.add_argument("-w", "--wordlist", help="Path to wordlist")
+parser.add_argument("-H", "--headers", help="Headers (optional)")
+parser.add_argument("-X", "--method", help="HTTP method to use (GET/POST/PUT/DELETE/...). Default: GET", default="GET")
+parser.add_argument("-d", "--data", help="Request data (e.g. 'a=1&b=2' or raw body). Optional.", default=None)
+parser.add_argument("-dir", "--dirfuzz", 
+                    nargs="?", const=True, default=False,
+                    help="Directory fuzzing mode (optional wordlist after flag)")
+parser.add_argument("-file", "--filefuzz", 
+                    nargs="?", const=True, default=False,
+                    help="File fuzzing mode (optional wordlist after flag)")
+
 
 # Parse them
 args = parser.parse_args()
 
 url = args.url
 wordlistPath=args.wordlist
+data=args.data
+mehtod=args.method
+header=args.headers
+dirFuzz= args.dirfuzz 
+fileFuzz= args.filefuzz 
 
 
-with open(f"{wordlistPath}","r") as file :
-    for line in file :
-        line=line.strip()
-        fURL=url.replace("FUZZ",line)
-        res=requests.get(f"{fURL}")
-        if res.status_code!=404:
-            print(f"Found: [{res.status_code}] {line} ")
-            print("-------------------------------------------------")
+def get_default_wordlist(mode):
+    defaults = {
+        "dir": "/usr/share/seclists/Discovery/Web-Content/raft-small-directories.txt",
+        "file": "/usr/share/seclists/Discovery/Web-Content/raft-small-files.txt",
+        "passwords":"~/rockyou.txt"
+    }
+    return defaults[mode]
+
+
+def search(wordlistPath):
+    with open(f"{wordlistPath}","r") as file :
+        for line in file :
+            line=line.strip()
+
+            if data :
+                if "FUZZ" in data:
+                    fURL=data.replace("FUZZ",line)   
+                    res=requests.request(method=mehtod,url=f"{fURL}",data=data,headers=header)
+            elif "FUZZ" in url :
+                fURL=url.replace("FUZZ",line)   
+                res=requests.request(method=mehtod,url=f"{fURL}",headers=header)
+            
+            if res.status_code!=404:
+                print(f"Found: [{res.status_code}] {line} ")
+                print("-"*60)
+
+
+def main():
+    init()
+    ascii_art = figlet_format('F U Z Z E R')
+    print(Fore.GREEN + ascii_art + Style.RESET_ALL)
+    print("-"*60)
+
+    if args.dirfuzz:
+        print("[+] Started directory fuzzing")
+        print("-"*60)
+
+        if args.dirfuzz is True:
+            wordlist = get_default_wordlist("dir")
+            print(f"Using default: {wordlist}")
+            print("-"*60)
+        else:
+            wordlist = args.dirfuzz
+            print(f"Using custom: {wordlist}")
+            print("-"*60)
+
+        search(wordlist)
+
+    if args.filefuzz:
+        print("[+] Started file fuzzing")
+        print("-"*60)
+
+        if args.filefuzz is True:
+            wordlist = get_default_wordlist("file")
+            print(f"Using default: {wordlist}")
+            print("-"*60)
+        else:
+            wordlist = args.filefuzz
+            print(f"Using custom: {wordlist}")
+            print("-"*60)
+
+        search(wordlist)
+
+    if not args.dirfuzz and not args.filefuzz:
+        print(f"{Fore.RED}[-] Error: Use -dir or -file mode!{Style.RESET_ALL}")
+    
+if __name__=="__main__":
+    main()
